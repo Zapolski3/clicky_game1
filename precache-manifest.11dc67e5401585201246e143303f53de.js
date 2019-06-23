8859abd105a80cedafc6144073cdc63f1e38883e
Pavel Zapolski <pavelzapolski@Pavels-MBP.fios-router.home>
2019-06-23 01:50:09 -0400

    Updates

---
+++ precache-manifest.11dc67e5401585201246e143303f53de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc073c8177dba62d6a0fdd1971a8380",
+    "url": "/clicky_game1/index.html"
+  },
+  {
+    "revision": "f0ff1545e7d1b550b869",
+    "url": "/clicky_game1/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clicky_game1/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f0ff1545e7d1b550b869",
+    "url": "/clicky_game1/static/js/main.ef8abe05.chunk.js"
+  },
+  {
+    "revision": "ba0f95591e6036311cd1",
+    "url": "/clicky_game1/static/js/runtime~main.3ec89338.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky_game1/static/media/logo.5d5d9eef.svg"
+  }
+]);
